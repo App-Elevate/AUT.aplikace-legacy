4b3a76baa72fb3405503bbff176a0822ba8cd2ba
mattheroit <verhaegenmatej@gmail.com>
2023-09-18 19:30:08 +0200

    Navigation drawer styling, code refactoring

--- lib/shared_widgets/navigation_drawer.dart
+++ lib/shared_widgets/navigation_drawer.dart
@@ -11,7 +11,6 @@ class MainAppDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final String username = getCanteenData().username;
     return Drawer(
       child: Scaffold(
         appBar: AppBar(
@@ -19,115 +18,63 @@ class MainAppDrawer extends StatelessWidget {
           title: const Text('Autojídelna'),
           centerTitle: true,
         ),
-        body: Column(
-          children: [
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ProfilePage(
-                      setHomeWidget: setHomeWidget,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: ListTile(
+                  title: const Text(
+                    'Jídelníček',
+                    style: TextStyle(
+                      fontSize: 20,
                     ),
                   ),
-                );
-              },
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Expanded(
-                    child: Container(
-                      color: MediaQuery.of(context).platformBrightness == Brightness.dark?const Color(0xff2e2e2e):const Color(0xfffafafa),
-                      child: Column(children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: FloatingActionButton(
-                            materialTapTargetSize: MaterialTapTargetSize.padded,
-                            backgroundColor: MediaQuery.of(context).platformBrightness == Brightness.dark?Colors.black:Colors.black,
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => ProfilePage(
-                                    setHomeWidget: setHomeWidget,
-                                  ),
-                                ),
-                              );
-                            },
-                            child: Icon(
-                              color: MediaQuery.of(context).platformBrightness == Brightness.dark?Colors.white:Colors.white,
-                              Icons.person),
-                          ),
-                        ),
-                        Center(
-                          child: Text(username),
-                        ),
-                      ]),
-                    ),
+                  leading: const Icon(
+                    Icons.restaurant,
+                    size: 30,
                   ),
-                ],
+                  selected: page == NavigationDrawerItem.jidelnicek,
+                  onTap: () async {
+                    if (page == NavigationDrawerItem.jidelnicek) {
+                      Navigator.pop(context);
+                    } else {
+                      Navigator.pop(context);
+                      await Future.delayed(const Duration(milliseconds: 200));
+                      setHomeWidget(
+                        MainAppScreen(
+                          setHomeWidget: setHomeWidget,
+                        ),
+                      );
+                    }
+                  },
+                ),
               ),
-            ),
-            const Divider(),
-            ListTile(
-              title: const Text('Jídelníček'),
-              leading: const Icon(Icons.restaurant),
-              selected: page == NavigationDrawerItem.jidelnicek,
-              onTap: () async {
-                if (page == NavigationDrawerItem.jidelnicek) {
-                  Navigator.pop(context);
-                } else {
-                  Navigator.pop(context);
-                  await Future.delayed(const Duration(milliseconds: 200));
-                  setHomeWidget(MainAppScreen(
-                    setHomeWidget: setHomeWidget,
-                  ));
-                }
-              },
-            ),
-            /*
-            ListTile(
-              title: const Text('Automatické objednávky'),
-              //icon that signilizes automatization
-              leading: const Icon(Icons.autorenew),
-              selected: page == NavigationDrawerItem.automatickeObjednavky,
-              onTap: () async {
-                if (page == NavigationDrawerItem.automatickeObjednavky) {
-                  Navigator.pop(context);
-                } else {
-                  Navigator.pop(context);
-                  await Future.delayed(const Duration(milliseconds: 200));
-                  setHomeWidget(AutoPage(
-                    setHomeWidget: setHomeWidget,
-                  ));
-                }
-              },
-            ),
-            ListTile(
-              title: const Text('Burza Catcher'),
-              //ikonka blesku
-              leading: const Icon(Icons.flash_on),
-              selected: page == NavigationDrawerItem.burzaCatcher,
-              onTap: () {
-                Fluttertoast.showToast(
-                    msg: "Tato Feature není hotová",
-                    toastLength: Toast.LENGTH_SHORT,
-                    timeInSecForIosWeb: 1,
-                    backgroundColor: const Color.fromARGB(255, 48, 48, 48),
-                    textColor: Colors.white,
-                    fontSize: 16.0);
-              },
-            ),*/
-          ],
-        ),
-        bottomNavigationBar: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            /*Padding(
-              padding: const EdgeInsets.only(right: 8.0, top: 8.0, left: 8.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  //alert the user it's not done yet using alerts not snackbar
+              /*
+              ListTile(
+                title: const Text('Automatické objednávky'),
+                //icon that signilizes automatization
+                leading: const Icon(Icons.autorenew),
+                selected: page == NavigationDrawerItem.automatickeObjednavky,
+                onTap: () async {
+                  if (page == NavigationDrawerItem.automatickeObjednavky) {
+                    Navigator.pop(context);
+                  } else {
+                    Navigator.pop(context);
+                    await Future.delayed(const Duration(milliseconds: 200));
+                    setHomeWidget(AutoPage(
+                      setHomeWidget: setHomeWidget,
+                    ));
+                  }
+                },
+              ),
+              ListTile(
+                title: const Text('Burza Catcher'),
+                //ikonka blesku
+                leading: const Icon(Icons.flash_on),
+                selected: page == NavigationDrawerItem.burzaCatcher,
+                onTap: () {
                   Fluttertoast.showToast(
                       msg: "Tato Feature není hotová",
                       toastLength: Toast.LENGTH_SHORT,
@@ -136,28 +83,9 @@ class MainAppDrawer extends StatelessWidget {
                       textColor: Colors.white,
                       fontSize: 16.0);
                 },
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(double.infinity, 50),
-                ),
-                child: const Text('Přepnout účet'),
-              ),
-            ),*/
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                onPressed: () => {
-                  logout(),
-                  setHomeWidget(LoginScreen(
-                    setHomeWidget: setHomeWidget,
-                  ))
-                },
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(double.infinity, 50),
-                ),
-                child: const Text('Odhlásit se'),
-              ),
-            ),
-          ],
+              ),*/
+            ],
+          ),
         ),
       ),
     );
