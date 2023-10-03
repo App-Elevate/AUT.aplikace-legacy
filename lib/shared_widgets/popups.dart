ce768e20e4ffa80b54be02a22887be3c3ef50326
tpkowastaken <protitom@gmail.com>
2023-10-03 22:07:22 +0200

    code reformating + gui improvements

--- lib/shared_widgets/popups.dart
+++ lib/shared_widgets/popups.dart
@@ -14,97 +14,124 @@ void newUpdateDialog(BuildContext context, {int? tries}) {
       return;
     }
   } catch (e) {
-    Future.delayed(const Duration(seconds: 1),
-        () => newUpdateDialog(context, tries: tries == null ? 1 : tries + 1));
+    Future.delayed(
+      const Duration(seconds: 1),
+      () => newUpdateDialog(context, tries: tries == null ? 1 : tries + 1),
+    );
   }
   showDialog(
     context: context,
     barrierDismissible: true,
     builder: (BuildContext context) {
       return AlertDialog(
-        title: const Text('Nová verze aplikace'),
-        content: SingleChildScrollView(
-          child: ListBody(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 4),
-                child: Text(
-                    'Vyšla nová verze aplikace - ${releaseInfo.latestVersion}'),
+        title: Text('Nová verze aplikace - ${releaseInfo.latestVersion}'),
+        content: SizedBox(
+          height: 200,
+          child: Scrollbar(
+            trackVisibility: true,
+            radius: const Radius.circular(20),
+            child: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    child: Text('Nová aktualizace přináší:'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 7.5, 0, 0),
+                    child: HtmlWidget(
+                      md.markdownToHtml(releaseInfo.changelog ??
+                          'Changelog není k dispozici'),
+                    ),
+                  ),
+                ],
               ),
-              const Text('Nová aktualizace přináší:'),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                child: HtmlWidget(
-                  md.markdownToHtml(
-                      releaseInfo.changelog ?? 'Changelog není k dispozici'),
-                ),
-              ),
-            ],
+            ),
           ),
         ),
         actionsAlignment: MainAxisAlignment.spaceBetween,
         actionsPadding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
         actions: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(horizontal: 3),
-                  //color based on brightness
-                  backgroundColor: MediaQuery.of(context).platformBrightness ==
-                          Brightness.dark
-                      ? const Color.fromARGB(20, 255, 255, 255)
-                      : const Color.fromARGB(20, 0, 0, 0),
-                ),
-                child: const Text('Aktualizovat'),
-                onPressed: () {
-                  Navigator.of(context).pop();
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    fixedSize: const Size.fromWidth(500),
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: const Text(
+                    'Aktualizovat',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17.5,
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
 
-                  PackageInfo.fromPlatform().then((value) {
-                    if (analyticsEnabledGlobally && analytics != null) {
-                      analytics!
-                          .logEvent(name: 'updateButtonClicked', parameters: {
-                        'oldVersion': value.version,
-                        'newVersion':
-                            releaseInfo.currentlyLatestVersion.toString()
-                      });
-                    }
-                  });
-                  networkInstallApk(releaseInfo.downloadUrl!, context);
-                },
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(horizontal: 3),
-                  //color based on brightness
-                  backgroundColor: MediaQuery.of(context).platformBrightness ==
-                          Brightness.dark
-                      ? const Color.fromARGB(20, 255, 255, 255)
-                      : const Color.fromARGB(20, 0, 0, 0),
+                    PackageInfo.fromPlatform().then(
+                      (value) {
+                        if (analyticsEnabledGlobally && analytics != null) {
+                          analytics!.logEvent(
+                            name: 'updateButtonClicked',
+                            parameters: {
+                              'oldVersion': value.version,
+                              'newVersion':
+                                  releaseInfo.currentlyLatestVersion.toString()
+                            },
+                          );
+                        }
+                      },
+                    );
+                    networkInstallApk(releaseInfo.downloadUrl!, context);
+                  },
                 ),
-                onPressed: (() => launchUrl(
-                    Uri.parse(
-                        "https://github.com/tpkowastaken/autojidelna/releases/tag/v${releaseInfo.latestVersion}"),
-                    mode: LaunchMode.externalApplication)),
-                child: const Text('Zobrazit na githubu'),
-              ),
-              ElevatedButton(
-                //make smaller space after the text
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(horizontal: 0),
-                  //color based on brightness
-                  backgroundColor: MediaQuery.of(context).platformBrightness ==
-                          Brightness.dark
-                      ? const Color.fromARGB(20, 255, 255, 255)
-                      : const Color.fromARGB(20, 0, 0, 0),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    fixedSize: const Size.fromWidth(500),
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    //color based on brightness
+                    backgroundColor:
+                        MediaQuery.of(context).platformBrightness ==
+                                Brightness.dark
+                            ? const Color.fromARGB(20, 255, 255, 255)
+                            : const Color.fromARGB(20, 0, 0, 0),
+                  ),
+                  onPressed: (() => launchUrl(
+                      Uri.parse(
+                          "https://github.com/tpkowastaken/autojidelna/releases/tag/v${releaseInfo.latestVersion}"),
+                      mode: LaunchMode.externalApplication)),
+                  child: const Text(
+                    'Zobrazit na githubu',
+                    style: TextStyle(fontSize: 17.5),
+                  ),
                 ),
-                child: const Text('Teď ne'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
+                TextButton(
+                  //make smaller space after the text
+                  style: TextButton.styleFrom(
+                    fixedSize: const Size.fromWidth(500),
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    //color based on brightness
+                    backgroundColor:
+                        MediaQuery.of(context).platformBrightness ==
+                                Brightness.dark
+                            ? const Color.fromARGB(20, 255, 255, 255)
+                            : const Color.fromARGB(20, 0, 0, 0),
+                  ),
+                  child: const Text(
+                    'Teď ne',
+                    style: TextStyle(fontSize: 17.5),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
           ),
         ],
       );
@@ -125,18 +152,20 @@ void failedLoginDialog(
         actionsPadding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
         actions: <Widget>[
           TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                setHomeWidget(LoggingInWidget(setHomeWidget: setHomeWidget));
-              },
-              child: const Text('Zkusit znovu')),
+            onPressed: () {
+              Navigator.of(context).pop();
+              setHomeWidget(LoggingInWidget(setHomeWidget: setHomeWidget));
+            },
+            child: const Text('Zkusit znovu'),
+          ),
           TextButton(
-              onPressed: () {
-                logout();
-                Navigator.of(context).pop();
-                setHomeWidget(LoginScreen(setHomeWidget: setHomeWidget));
-              },
-              child: const Text('Odhlásit se')),
+            onPressed: () {
+              logout();
+              Navigator.of(context).pop();
+              setHomeWidget(LoginScreen(setHomeWidget: setHomeWidget));
+            },
+            child: const Text('Odhlásit se'),
+          ),
         ],
       );
     },
@@ -156,16 +185,18 @@ void failedDownload(BuildContext context) async {
         actionsPadding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
         actions: <Widget>[
           TextButton(
-              onPressed: () {
-                networkInstallApk(releaseInfo.downloadUrl!, context);
-                Navigator.of(context).pop();
-              },
-              child: const Text('Zkusit znovu')),
+            onPressed: () {
+              networkInstallApk(releaseInfo.downloadUrl!, context);
+              Navigator.of(context).pop();
+            },
+            child: const Text('Zkusit znovu'),
+          ),
           TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Zrušit')),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Zrušit'),
+          ),
         ],
       );
     },
