9fb62d1cc200baf367b82dedafe2e2baf81beb23
tpkowastaken <protitom@gmail.com>
2024-01-22 12:48:35 +0100

    renamed jidlo -> jidloDetail.dart

--- lib/pages/all.dart
+++ lib/pages/all.dart
@@ -2,5 +2,5 @@ export 'jidelna.dart';
 export 'login.dart';
 export 'profile.dart';
 export 'settings.dart';
-export 'jidlo.dart';
+export 'jidloDetail.dart';
 export 'needed_install_permission.dart';
