adbc7336d60a1bf19edb4e8f6f259ad50c3e6fa6
mattheroit <verhaegenmatej@gmail.com>
2024-05-13 14:02:49 +0200

    move appearance settings to seperate screen

---
+++ lib/pages_new/appearance.dart
@@ -0,0 +1,52 @@
+import 'package:autojidelna/providers.dart';
+import 'package:autojidelna/shared_widgets/settings/all_settings_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class AppearanceScreen extends StatelessWidget {
+  const AppearanceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final userPreferences = context.watch<UserPreferences>();
+    final bool isBright = MediaQuery.platformBrightnessOf(context) == Brightness.light || userPreferences.themeMode == ThemeMode.light;
+    final TextStyle? subtitleTextStyle = isBright ? null : const TextStyle(color: Colors.white54);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Appearance")),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const ThemeModePicker(),
+            const ThemeStylePicker(),
+            SwitchListTile(
+              title: const Text("Pure black dark mode"),
+              subtitle: Text("If You Only Knew The Power Of The Dark Side...", style: subtitleTextStyle),
+              value: userPreferences.isPureBlack,
+              onChanged: isBright ? null : (value) => userPreferences.setPureBlack = value,
+            ),
+            // TODO: implement this
+            SwitchListTile(
+              title: const Text("List UI"),
+              subtitle: Text("Old School!!!", style: subtitleTextStyle),
+              value: userPreferences.isListUi,
+              onChanged: (value) => userPreferences.setListUi = value,
+            ),
+            // TODO: implement this
+            SwitchListTile(
+              title: const Text("Big calendar markers"),
+              value: userPreferences.bigCalendarMarkers,
+              onChanged: (value) => userPreferences.setCalendarMarkers = value,
+            ),
+            // TODO: implement this
+            SwitchListTile(
+              title: const Text("Skip weekends"),
+              value: userPreferences.skipWeekends,
+              onChanged: (value) => userPreferences.setSkipWeekends = value,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
