a499c7ba9aa37663aa03e5d92e18f6123ad7ab23
mattheroit <verhaegenmatej@gmail.com>
2024-05-13 19:02:24 +0200

    centralize shared preferences R/W functions

---
+++ lib/shared_prefs_functions.dart
@@ -0,0 +1,51 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+void saveIntToSharedPreferences(String key, int value) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setInt(key, value);
+}
+
+void saveBoolToSharedPreferences(String key, bool value) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setBool(key, value);
+}
+
+void saveDoubleToSharedPreferences(String key, double value) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setDouble(key, value);
+}
+
+void saveStringToSharedPreferences(String key, String value) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setString(key, value);
+}
+
+void saveStringListToSharedPreferences(String key, List<String> value) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setStringList(key, value);
+}
+
+Future<int?> readIntFromSharedPreferences(String key) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getInt(key);
+}
+
+Future<bool?> readBoolFromSharedPreferences(String key) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getBool(key);
+}
+
+Future<double?> readDoubleFromSharedPreferences(String key) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getDouble(key);
+}
+
+Future<String?> readStringFromSharedPreferences(String key) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getString(key);
+}
+
+Future<List<String>?> readListStringFromSharedPreferences(String key) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getStringList(key);
+}
