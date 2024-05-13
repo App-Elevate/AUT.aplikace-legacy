4ba8df53044b62ead578cb16c5c24e8a73d45594
mattheroit <verhaegenmatej@gmail.com>
2024-05-13 10:47:18 +0200

    add themeMode picker to new settings page

--- lib/pages_new/settings.dart
+++ lib/pages_new/settings.dart
@@ -1,4 +1,6 @@
+import 'package:autojidelna/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SettingAppBar extends StatelessWidget implements PreferredSizeWidget {
   const SettingAppBar({super.key});
@@ -17,6 +19,23 @@ class SettingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Consumer<UserPreferences>(
+            builder: (context, value, child) => SegmentedButton<ThemeMode>(
+              showSelectedIcon: false,
+              selected: {value.themeMode},
+              onSelectionChanged: (Set<ThemeMode> selected) => value.setThemeMode = selected.first,
+              segments: const [
+                ButtonSegment<ThemeMode>(value: ThemeMode.system, label: Text("System")),
+                ButtonSegment<ThemeMode>(value: ThemeMode.light, label: Text("Light")),
+                ButtonSegment<ThemeMode>(value: ThemeMode.dark, label: Text("Dark")),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
