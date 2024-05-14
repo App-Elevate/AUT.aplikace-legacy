9aeb737ab048cf77af84892bb60a02ea3335747a
mattheroit <verhaegenmatej@gmail.com>
2024-05-14 11:09:20 +0200

    create spacing.dart

---
+++ lib/classes_enums/spacing.dart
@@ -0,0 +1,17 @@
+/// Standardised doubles for spacing in UI
+class Spacing {
+  /// 0
+  static double get zero => 0;
+
+  /// 4.0
+  static double get short1 => 4;
+
+  /// 8.0
+  static double get short2 => 8;
+
+  /// 30.0
+  static double get medium1 => 30;
+
+  /// 38.0
+  static double get medium2 => 38;
+}
