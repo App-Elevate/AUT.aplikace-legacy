ddc071f95fe92c9fa721334a9d112b7273dd80b6
Matěj <matej@appelevate.cz>
2024-05-19 03:38:31 +0200

    create about screen

---
+++ lib/pages_new/about.dart
@@ -0,0 +1,82 @@
+import 'package:autojidelna/shared_widgets/settings/custom_divider.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:icons_flutter/icons_flutter.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("About")),
+      body: Column(
+        children: [
+          // logo
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 85.0),
+            child: SvgPicture.asset(
+              "assets/images/logo.svg",
+              colorFilter: ColorFilter.mode(Theme.of(context).colorScheme.onBackground, BlendMode.srcIn),
+              height: MediaQuery.sizeOf(context).height * .10,
+            ),
+          ),
+          const CustomDivider(isTransparent: false),
+          // version list tile
+          FutureBuilder(
+            future: PackageInfo.fromPlatform(),
+            builder: (context, data) {
+              if (data.hasData && data.data != null) {
+                return ListTile(
+                  title: const Text("Version"),
+                  subtitle: Text("${kDebugMode ? "Debug" : "Stable"} ${data.data!.version}"),
+                  onTap: () async {
+                    await Clipboard.setData(
+                      ClipboardData(text: "App version: ${data.data!.version}"),
+                    ); // TODO: mby add some other data
+                  },
+                );
+              }
+              return const SizedBox();
+            },
+          ),
+          // licenses list tile
+          ListTile(
+            title: const Text("Licenses"),
+            onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => const LicensePage())),
+          ),
+          // collected data list tile
+          ListTile(
+            title: const Text("Collected data list"),
+            onTap: () {
+              launchUrl(Uri.parse("https://github.com/Autojidelna/autojidelna/blob/main/listSbiranychDat.md"));
+            },
+          ),
+          const CustomDivider(isTransparent: false),
+          // links
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                onPressed: () => launchUrl(Uri.parse("https://autojidelna.cz")),
+                icon: const Icon(Icons.public_outlined),
+              ),
+              IconButton(
+                onPressed: () => launchUrl(Uri.parse("https://github.com/Autojidelna/autojidelna")),
+                icon: const Icon(Octicons.mark_github),
+              ),
+              IconButton(
+                onPressed: () => launchUrl(Uri(scheme: "mailto", path: "autojidelna@tomprotiva.com")),
+                icon: const Icon(Icons.email_outlined),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
