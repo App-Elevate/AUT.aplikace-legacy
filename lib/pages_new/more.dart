73cccadce4f3881d5ed55ac23afab9d2eabecb7f
mattheroit <verhaegenmatej@gmail.com>
2024-05-20 13:06:18 +0200

    use account overview and location picker cards

--- lib/pages_new/more.dart
+++ lib/pages_new/more.dart
@@ -1,8 +1,9 @@
 import 'package:autojidelna/classes_enums/spacing.dart';
 import 'package:autojidelna/pages_new/about.dart';
 import 'package:autojidelna/pages_new/settings.dart';
+import 'package:autojidelna/shared_widgets/more/account_overview_card.dart';
+import 'package:autojidelna/shared_widgets/more/location_picker_card.dart';
 import 'package:autojidelna/shared_widgets/settings/custom_divider.dart';
-import 'package:autojidelna/shared_widgets/switch_account_panel_v2.dart';
 import 'package:flutter/material.dart';
 
 class MoreAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -23,104 +24,9 @@ class MorePage extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: [
-          Card(
-            clipBehavior: Clip.hardEdge,
-            margin: EdgeInsets.symmetric(horizontal: Spacing.shortMedium),
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: Spacing.shortMedium, vertical: Spacing.short1),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Row(
-                    children: [
-                      Text("username", style: TextStyle(color: Colors.white54)),
-                      Flexible(child: CustomDivider(isTransparent: false, hasEndIndent: false)),
-                    ],
-                  ),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Icon(Icons.account_circle, size: 75),
-                      VerticalDivider(color: Colors.transparent),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Credit: 2000 kč"),
-                          Text("Category: Students"),
-                        ],
-                      ),
-                    ],
-                  ),
-                  MaterialButton(
-                    visualDensity: const VisualDensity(vertical: -4),
-                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                    padding: EdgeInsets.zero,
-                    splashColor: Colors.transparent,
-                    focusColor: Colors.transparent,
-                    highlightColor: Colors.transparent,
-                    textColor: Theme.of(context).colorScheme.primary,
-                    onPressed: () {
-                      showModalBottomSheet(
-                        context: context,
-                        useRootNavigator: true,
-                        clipBehavior: Clip.hardEdge,
-                        constraints: BoxConstraints(maxHeight: MediaQuery.sizeOf(context).height * .6),
-                        builder: (context) => const SwitchAccountPanelV2(),
-                      );
-                    },
-                    child: const Row(
-                      children: [
-                        Flexible(child: CustomDivider(isTransparent: false, hasIndent: false)),
-                        Text("Change account", style: TextStyle(color: Colors.white54)),
-                        Flexible(child: CustomDivider(isTransparent: false, hasEndIndent: false)),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          const AccountOverviewCard(),
           const CustomDivider(),
-          Card(
-            clipBehavior: Clip.hardEdge,
-            margin: EdgeInsets.symmetric(horizontal: Spacing.shortMedium),
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: Spacing.shortMedium, vertical: Spacing.short1),
-              child: Column(
-                children: [
-                  const Row(
-                    children: [
-                      Text("Location", style: TextStyle(color: Colors.white54)),
-                      Flexible(child: CustomDivider(isTransparent: false, hasEndIndent: false)),
-                    ],
-                  ),
-                  const ListTile(
-                    contentPadding: EdgeInsets.zero,
-                    visualDensity: VisualDensity(vertical: -4),
-                    title: Text("unknown"),
-                  ),
-                  MaterialButton(
-                    visualDensity: const VisualDensity(vertical: -4),
-                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                    padding: EdgeInsets.zero,
-                    splashColor: Colors.transparent,
-                    focusColor: Colors.transparent,
-                    highlightColor: Colors.transparent,
-                    textColor: Theme.of(context).colorScheme.primary,
-                    onPressed: () {},
-                    child: const Row(
-                      children: [
-                        Flexible(child: CustomDivider(isTransparent: false, hasIndent: false)),
-                        Text("Change location", style: TextStyle(color: Colors.white54)),
-                        Flexible(child: CustomDivider(isTransparent: false, hasEndIndent: false)),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          const LocationPickerCard(),
           CustomDivider(height: Spacing.medium2),
           ListTile(
             leading: const Icon(Icons.settings_outlined),
