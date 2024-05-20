b678939ae61105d092773c283d16491c7010d552
mattheroit <verhaegenmatej@gmail.com>
2024-05-20 13:55:02 +0200

    use new login screen as home screen

--- lib/pages_new/login.dart
+++ lib/pages_new/login.dart
@@ -1,10 +1,22 @@
+import 'package:autojidelna/pages_new/navigation.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class LoginScreenV2 extends StatelessWidget {
+  const LoginScreenV2({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(forceMaterialTransparency: true));
+    return Scaffold(
+      appBar: AppBar(forceMaterialTransparency: true),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Log In"),
+          onPressed: () => Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => const NavigationScreen()),
+            (route) => false,
+          ),
+        ),
+      ),
+    );
   }
 }
