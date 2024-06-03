eed3286f2706c88aaf9639de00e39ab19d40b47a
tpkowastaken <protitom@gmail.com>
2024-06-03 14:09:37 +0200

    awesome notifications work

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,21 +1,21 @@
+import UIKit
 import Flutter
 import awesome_notifications
-import shared_preferences_ios
-//import all_other_plugins_that_i_need
+import shared_preferences_foundation
 
-override func application(
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
       GeneratedPluginRegistrant.register(with: self)
-
-      // This function registers the desired plugins to be used within a notification background action
       SwiftAwesomeNotificationsPlugin.setPluginRegistrantCallback { registry in          
           SwiftAwesomeNotificationsPlugin.register(
-            with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)          
-          FLTSharedPreferencesPlugin.register(
+            with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)
+          SharedPreferencesPlugin.register(
             with: registry.registrar(forPlugin: "io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin")!)
       }
-
       return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+}
