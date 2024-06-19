c67cb99888448075922599f29d92d745924c49a2
Matěj <matej@appelevate.cz>
2024-06-19 09:19:35 +0200

    move notifications to own screen

---
+++ lib/pages_new/notifications.dart
@@ -0,0 +1,40 @@
+import 'package:autojidelna/providers.dart';
+import 'package:autojidelna/shared_widgets/settings/section_title.dart';
+import 'package:autojidelna/shared_widgets/settings/time_picker_todays_food.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class NotificationScreen extends StatelessWidget {
+  const NotificationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Notifications")),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SectionTitle("Notifications"),
+            const TimePickerTodaysFoodTiles(),
+            Selector<NotificationPreferences, ({bool read, Function(bool) set})>(
+              selector: (_, p1) => (read: p1.lowCredit, set: p1.setLowCredit),
+              builder: (context, lowCredit, child) => SwitchListTile(
+                title: const Text("Low Credit"),
+                value: lowCredit.read,
+                onChanged: lowCredit.set,
+              ),
+            ),
+            Selector<NotificationPreferences, ({bool read, Function(bool) set})>(
+              selector: (_, p1) => (read: p1.weekLongFamine, set: p1.setWeekLongFamine),
+              builder: (context, weekLongFamine, child) => SwitchListTile(
+                title: const Text("No food"),
+                value: weekLongFamine.read,
+                onChanged: weekLongFamine.set,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages_new/settings.dart
+++ lib/pages_new/settings.dart
@@ -1,10 +1,10 @@
 import 'package:autojidelna/classes_enums/spacing.dart';
 import 'package:autojidelna/pages_new/appearance.dart';
 import 'package:autojidelna/pages_new/data_collection.dart';
+import 'package:autojidelna/pages_new/notifications.dart';
 import 'package:autojidelna/providers.dart';
 import 'package:autojidelna/shared_widgets/settings/custom_divider.dart';
 import 'package:autojidelna/shared_widgets/settings/section_title.dart';
-import 'package:autojidelna/shared_widgets/settings/time_picker_todays_food.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -23,6 +23,11 @@ class SettingsScreen extends StatelessWidget {
               leading: const Icon(Icons.palette_outlined),
               title: const Text("Appearance"),
               onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => const AppearanceScreen())),
+            ),
+            ListTile(
+              leading: const Icon(Icons.edit_notifications_outlined),
+              title: const Text("Notifications"),
+              onTap: () async => Navigator.of(context).push(MaterialPageRoute(builder: (context) => const NotificationScreen())),
             ),
             ListTile(
               leading: const Icon(Icons.cookie_outlined),
@@ -48,25 +53,6 @@ class SettingsScreen extends StatelessWidget {
                 title: const Text("Big calendar markers"),
                 value: bigCalendarMarkers.read,
                 onChanged: bigCalendarMarkers.set,
-              ),
-            ),
-            // TODO: implement this
-            const SectionTitle("Notification"),
-            const TimePickerTodaysFoodTiles(),
-            Selector<NotificationPreferences, ({bool read, Function(bool) set})>(
-              selector: (_, p1) => (read: p1.lowCredit, set: p1.setLowCredit),
-              builder: (context, lowCredit, child) => SwitchListTile(
-                title: const Text("Low Credit"),
-                value: lowCredit.read,
-                onChanged: lowCredit.set,
-              ),
-            ),
-            Selector<NotificationPreferences, ({bool read, Function(bool) set})>(
-              selector: (_, p1) => (read: p1.weekLongFamine, set: p1.setWeekLongFamine),
-              builder: (context, weekLongFamine, child) => SwitchListTile(
-                title: const Text("No food"),
-                value: weekLongFamine.read,
-                onChanged: weekLongFamine.set,
               ),
             ),
           ],
