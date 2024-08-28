08df8e90d312fb1e8cfc9d24d87fd20894b52501
Matěj Verhaegen <matej@appelevate.cz>
2024-08-28 14:30:10 +0200

    add notification perms request to navigation.dart

--- lib/pages_new/navigation.dart
+++ lib/pages_new/navigation.dart
@@ -1,5 +1,10 @@
+import 'package:autojidelna/consts.dart';
+import 'package:autojidelna/methods_vars/canteenwrapper.dart';
+import 'package:autojidelna/methods_vars/notifications.dart';
 import 'package:autojidelna/pages_new/canteen.dart';
 import 'package:autojidelna/pages_new/more/more.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:background_fetch/background_fetch.dart';
 import 'package:flutter/material.dart';
 
 class NavigationScreen extends StatefulWidget {
@@ -13,6 +18,27 @@ class _NavigationScreenState extends State<NavigationScreen> {
   int pageIndex = 0;
 
   void changePageIndex(int index) => setState(() => pageIndex = index);
+
+  @override
+  initState() {
+    loggedInCanteen.readData(Prefs.firstTime).then((value) {
+      if (value != '1') {
+        initPlatformState().then((value) async {
+          await AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+            if (!isAllowed) {
+              //TODO: less annoying popup for notifications - show a friendly screen before asking
+              AwesomeNotifications().requestPermissionToSendNotifications();
+            }
+            BackgroundFetch.start();
+          });
+        });
+      } else {
+        initPlatformState();
+      }
+      loggedInCanteen.saveData(Prefs.firstTime, '1');
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
