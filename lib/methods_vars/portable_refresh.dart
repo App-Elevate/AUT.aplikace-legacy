f4bbeb96cd2bc182c6272dd10d7175b0a9887a6a
Matěj Verhaegen <matej@appelevate.cz>
2024-09-06 19:04:24 +0200

    working portable refresh

--- lib/methods_vars/portable_refresh.dart
+++ lib/methods_vars/portable_refresh.dart
@@ -1,23 +1,25 @@
 import 'package:autojidelna/lang/l10n_global.dart';
-import 'package:autojidelna/main.dart';
-import 'package:autojidelna/methods_vars/canteenwrapper.dart';
-import 'package:autojidelna/methods_vars/widgets_tracking.dart';
-import 'package:autojidelna/shared_widgets/snackbar.dart';
+import 'package:autojidelna/local_imports.dart';
+import 'package:autojidelna/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 Future<void> portableSoftRefresh() async {
-  try {
-    await loggedInCanteen.getLunchesForDay(dateListener.value, requireNew: true);
-  } catch (e) {
-    // Find the ScaffoldMessenger in the widget tree
-    // and use it to show a SnackBar.
-    BuildContext? ctx = MyApp.navigatorKey.currentContext;
-    if (ctx != null && ctx.mounted && !snackbarshown.shown) {
-      ScaffoldMessenger.of(ctx).showSnackBar(snackbarFunction(lang.errorsUpdatingData)).closed.then(
-        (SnackBarClosedReason reason) {
-          snackbarshown.shown = false;
-        },
-      );
+  BuildContext? ctx = MyApp.navigatorKey.currentContext;
+  DateTime day = dateListener.value;
+  if (ctx != null) {
+    try {
+      ctx.read<DishesOfTheDay>().setMenu(convertDateTimeToIndex(day), await loggedInCanteen.getLunchesForDay(day, requireNew: true));
+    } catch (e) {
+      // Find the ScaffoldMessenger in the widget tree
+      // and use it to show a SnackBar.
+      if (ctx.mounted && !snackbarshown.shown) {
+        ScaffoldMessenger.of(ctx).showSnackBar(snackbarFunction(lang.errorsUpdatingData)).closed.then(
+          (SnackBarClosedReason reason) {
+            snackbarshown.shown = false;
+          },
+        );
+      }
     }
   }
 }
